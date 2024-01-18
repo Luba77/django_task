670306bde4f8d98885ca8a8599979db88f8db09c
Luba77 <lubaandpython@gmail.com>
2024-01-18 13:56:56 +0200

    start project, created models, viewsets, urls

---
+++ people/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
+
