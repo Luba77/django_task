f9bb8bb05b01923307b7b37d6307b79fe01f3acf
Luba77 <lubaandpython@gmail.com>
2024-01-18 14:36:06 +0200

    added PersonDetailView, PersonListView and create, update, delete methods, added new urls

--- people/urls.py
+++ people/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TeamListCreateView
+from .views import TeamListCreateView, TeamDetailView, PersonListCreateView, PersonDetailView
 
 urlpatterns = [
     path('teams/', TeamListCreateView.as_view(), name='team-list-create'),
+    path('teams/<int:pk>/', TeamDetailView.as_view(), name='team-detail'),
+    path('person/', PersonListCreateView.as_view(), name='person-list-create'),
+    path('person/<int:pk>/', PersonDetailView.as_view(), name='person-detail'),
 ]
 
--- people/views.py
+++ people/views.py
@@ -8,3 +8,18 @@ class TeamListCreateView(generics.ListCreateAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
+
+class TeamDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class PersonListCreateView(generics.ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+
+class PersonDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
